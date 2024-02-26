53a3913ddc17a46c514b54e7fc22254814c3035e
Diana Alonss <dianaalonsoaransay@gmail.com>
2024-02-26 13:08:49 +0100

    cambios_mejoras

--- src/app/views/asistencia-view/asistencia-view.component.ts
+++ src/app/views/asistencia-view/asistencia-view.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../core/header/header.component';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-asistencia-view',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, RouterModule],
   templateUrl: './asistencia-view.component.html',
   styleUrl: './asistencia-view.component.scss'
 })
